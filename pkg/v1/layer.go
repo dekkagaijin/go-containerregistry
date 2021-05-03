768177d2b56dee7e6ac72f65c5d9584bcf772611
Jake Sanders <jsand@google.com>
2021-05-03 16:21:46 -0700

    Add StaticLayer
    Signed-off-by: Jake Sanders <jsand@google.com>

--- pkg/v1/layer.go
+++ pkg/v1/layer.go
@@ -15,7 +15,9 @@
 package v1
 
 import (
+	"bytes"
 	"io"
+	"io/ioutil"
 
 	"github.com/google/go-containerregistry/pkg/v1/types"
 )
@@ -39,4 +41,54 @@ type Layer interface {
 
 	// MediaType returns the media type of the Layer.
 	MediaType() (types.MediaType, error)
+}
+
+// StaticLayer returns a Layer which references a static payload.
+// `Compressed` and `Uncompressed` are equivalent, returning a Reader which returns the raw contents.
+// `Digest` and `DiffID` are similarly equivalent, returning the SHA256 Hash of the raw contents.
+func StaticLayer(contents []byte, mediaType types.MediaType) (Layer, error) {
+	h, _, err := SHA256(bytes.NewReader(contents))
+	if err != nil {
+		return nil, err
+	}
+	return &staticLayer{
+		b:  contents,
+		h:  h,
+		mt: mediaType,
+	}, nil
+}
+
+type staticLayer struct {
+	b  []byte
+	h  Hash
+	mt types.MediaType
+}
+
+func (l *staticLayer) Digest() (Hash, error) {
+	return l.h, nil
+}
+
+// DiffID returns the Hash of the uncompressed layer.
+func (l *staticLayer) DiffID() (Hash, error) {
+	return l.h, nil
+}
+
+// Compressed returns an io.ReadCloser for the compressed layer contents.
+func (l *staticLayer) Compressed() (io.ReadCloser, error) {
+	return ioutil.NopCloser(bytes.NewReader(l.b)), nil
+}
+
+// Uncompressed returns an io.ReadCloser for the uncompressed layer contents.
+func (l *staticLayer) Uncompressed() (io.ReadCloser, error) {
+	return ioutil.NopCloser(bytes.NewReader(l.b)), nil
+}
+
+// Size returns the compressed size of the Layer.
+func (l *staticLayer) Size() (int64, error) {
+	return int64(len(l.b)), nil
+}
+
+// MediaType returns the media type of the Layer.
+func (l *staticLayer) MediaType() (types.MediaType, error) {
+	return l.mt, nil
 }
